593942168e6c90f8c0c31586013432069a242307
mkcochran <mcochran@redhat.com>
2016-08-23 21:18:34 -0400

    hello world example working

---
+++ hello-world/src/main/java/com/example/fusequickstarts/karaf/deployment/route/SampleRoute.java
@@ -0,0 +1,19 @@
+package com.example.fusequickstarts.karaf.deployment.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+
+public class SampleRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // @formatter:off
+        
+        // Logs Hello World every 2000 milliseconds
+        from("timer://myTimer?fixedRate=true&period=2000")
+            .log(LoggingLevel.INFO, "com.redhat.consulting.fusequickstarts.karaf.deployment.route", "Hello World")
+            .to("log:HelloWorldLog?level=INFO");
+
+    }
+
+}
