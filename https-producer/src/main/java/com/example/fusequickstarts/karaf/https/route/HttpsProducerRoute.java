e9a82ba5fb1a2272deb57f5ddd827bdd31c4215a
mkcochran <mcochran@redhat.com>
2016-08-24 20:41:39 -0400

    https producer example started

---
+++ https-producer/src/main/java/com/example/fusequickstarts/karaf/https/route/HttpsProducerRoute.java
@@ -0,0 +1,16 @@
+package com.example.fusequickstarts.karaf.https.route;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class HttpsProducerRoute extends RouteBuilder {
+	
+    @Override
+    public void configure() throws Exception {
+        // @formatter:off
+        from("timer://myHttpsTimer?fixedRate=true&period=2000")
+            .routeId("https-producer")
+            //TODO make https
+            .to("jetty:http://127.0.0.1:8182/sample");
+    }
+
+}
